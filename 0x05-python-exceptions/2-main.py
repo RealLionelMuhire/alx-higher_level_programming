ff35ced404176069cdd9d9b9554db68f14789769
RealLionelMuhire <muhirelionel@gmail.com>
2022-10-12 14:58:39 +0200

    list int

--- 0x05-python-exceptions/2-main.py
+++ 0x05-python-exceptions/2-main.py
@@ -1,4 +1,4 @@
 safe_print_list_integers = __import__('2-safe_print_list_integers').safe_print_list_integers
-my_list = [1, 2, 2, 4, 5, 5]
-rar = safe_print_list_integers(my_list, 2)
+my_list = [1, 2, 2, 4,"hello", 5, 5]
+rar = safe_print_list_integers(my_list, 7)
 print("{}".format(rar))
