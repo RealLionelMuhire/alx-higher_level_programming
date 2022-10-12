6f5f77a2bfe4d08c8d84259d68ccdf2c7ac4ffb3
RealLionelMuhire <muhirelionel@gmail.com>
2022-10-12 14:35:08 +0200

    Try/Except numbers

---
+++ 0x05-python-exceptions/2-main.py
@@ -0,0 +1,4 @@
+safe_print_list_integers = __import__('2-safe_print_list_integers').safe_print_list_integers
+my_list = [1, 2, 2, 4, 5, 5]
+rar = safe_print_list_integers(my_list, 2)
+print("{}".format(rar))
