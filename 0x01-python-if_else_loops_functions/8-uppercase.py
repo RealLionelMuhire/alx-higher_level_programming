85c54e7bda4cdcb5392166fada5be1efd88768d1
RealLionelMuhire <muhirelionel@gmail.com>
2023-06-07 16:48:25 +0200

    entering formats

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     for c in str:
         if ord(c) in range(97, 123):
             c = chr(ord(c) - 32)
-            print(c, end='')
+            print("{}".format(c), end='')
         else:
-            print(c, end='')
+            print("{}".format(c), end='')
     print('\n')
