9291d2926dfeb45cb76e3d812f58afffe573e858
RealLionelMuhire <muhirelionel@gmail.com>
2023-06-08 15:37:51 +0200

    name main

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-from sys import argv
-ln = len(argv)
-sum = 0
-int(sum)
-if ln < 2:
-    print("{}".format(sum))
-else:
-    n = 1
-    while n < ln:
-        sum += int(argv[n])
-        n += 1
-    print("{}".format(sum))
+if __name__ == "__main__":
+    from sys import argv
+    ln = len(argv)
+    sum = 0
+    int(sum)
+    if ln < 2:
+        print("{}".format(sum))
+    else:
+        n = 1
+        while n < ln:
+            sum += int(argv[n])
+            n += 1
+        print("{}".format(sum))
