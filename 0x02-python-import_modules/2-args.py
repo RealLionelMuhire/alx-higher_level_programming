1f1afcb769d7afa03234b3595bea1ef3f1124846
RealLionelMuhire <muhirelionel@gmail.com>
2023-06-08 14:41:09 +0200

    argumentation

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    arg_len = len(sys.argv)
-    if arg_len == 1:
-        print("{} arguments.".format(arg_len - 1))
-    if arg_len == 2:
-        print("{} argument.".format(arg_len - 1))
-        print("{}: {}".format(1, sys.argv[1]))
-    elif arg_len > 2:
-        print("{} arguments:".format(arg_len - 1))
-        n = 0
-        for arg in range(arg_len):
+    from sys import argv
+    ln = len(argv)
+    if ln == 1:
+        print("{} arguments.".format(ln - 1))
+    if ln == 2:
+        print("{} argument:".format(ln - 1))
+        print("{}: {}".format(1, argv[1]))
+    elif ln > 2:
+        print("{} arguments:".format(ln - 1))
+        n = 1
+        while n < ln:
+            print("{}: {}".format(n, argv[n]))
             n += 1
-            if n > arg_len - 1:
-                break
-            print("{}: {}".format(n, sys.argv[n]))
