ac47d689825a05bc10da03ccc8a3fa2af4e37099
RealLionelMuhire <muhirelionel@gmail.com>
2023-06-10 01:00:59 +0200

    matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for item in matrix:
+        for subitem in item:
+            if item.index(subitem) < len(item) - 1:
+                print("{:d}".format(subitem), end=" ")
+            else:
+                print("{:d}".format(subitem))
+    print()
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
