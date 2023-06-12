a0589066263749f3e88cefdd315c0bc5d909e32a
RealLionelMuhire <muhirelionel@gmail.com>
2023-06-12 23:15:08 +0200

    pallindome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,3 +1,42 @@
 #include "lists.h"
 
+/**
+ * is_palindrome - checks wether a linked list is palindome
+ * @head: head node
+ * Return: 1 if is pallindome otherwise 0
+ */
 
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow, *fast, *past = NULL, *prev = NULL;
+	slow = *head;
+	fast = *head;
+
+	if (!(*head))
+		return (1);
+
+	while(fast)
+	{
+		fast = fast->next->next;
+		prev = slow;
+		slow = slow->next;
+	}
+	while(slow)
+	{
+		past = slow->next;
+		slow->next = prev;
+		prev = slow;
+		slow = past;
+	}
+	slow = prev;
+	fast = *head;
+
+	while (slow != NULL)
+	{
+		if (slow->n != fast->n)
+			return (0);
+		slow = slow->next;
+		fast = fast->next;
+	}
+	return (1);
+}
