9fbbefb1c4ae92be92d5df5809e3edf17d4ba223
RealLionelMuhire <muhirelionel@gmail.com>
2023-06-15 00:02:25 +0200

    remoivng unnecessary lambda and map

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     n_dict = {}
     for key, value in a_dictionary.items():
-        n_dict[key] = list(map(lambda x: x * 2, value))
+        n_dict[key] = value * 2
     return n_dict
