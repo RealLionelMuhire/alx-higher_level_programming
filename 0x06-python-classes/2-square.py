d326435b6f099740570d76397ad23b22caf4203f
RealLionelMuhire <muhirelionel@gmail.com>
2023-06-28 23:57:21 +0200

    classinitialiser

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Create a class with size as attribute"""
+
+
+class Square:
+    """class of square"""
+    def __init__(self, size=0):
+        """initialization of Square and raise some errors
+        args:
+            size"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
