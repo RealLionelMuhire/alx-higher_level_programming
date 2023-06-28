9bcb739274112bbd755fb7b76940530c3d967bfc
RealLionelMuhire <muhirelionel@gmail.com>
2023-06-29 01:00:25 +0200

    adding a print empty on zero passed

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -41,6 +41,9 @@ class Square:
 
     def my_print(self):
         """prints in stdout the square with the character #"""
+        if self.__size == 0:
+            print("")
+            return
         i = 0
         for i in range(self.__size):
             j = 0
