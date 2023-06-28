0fe0feac67228b9db96850bf656b7a72b7771dd1
RealLionelMuhire <muhirelionel@gmail.com>
2023-06-29 00:12:03 +0200

    computing an area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
-class Square
+"""Create a class with size as attribute"""
+
+
+class Square:
+    """class of square"""
+    def __init__(self, size=0):
+        """initialization of Square and raise some errors
+        args:
+            size"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    
+    def area(self):
+        """area of a circle
+        args:
+            None
+        Return: the area of the circle"""
+        return self.__size * self.__size
