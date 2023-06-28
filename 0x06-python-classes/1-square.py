45eb139fa4bed79cc68b50708c4c35c09c5f0e18
RealLionelMuhire <muhirelionel@gmail.com>
2023-06-28 23:28:46 +0200

    initialization of size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Create a class with size as attribute of instance"""
+
+
+class Square:
+    """Class of a square"""
+    
+    def __init__(self, size):
+        """initialise the class, with its size
+        args:
+            size: size of the square"""
+        self.__size = size
