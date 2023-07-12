4b4db278f3abf9cf27e4ef5e1b0a487d06bd07f6
RealLionelMuhire <muhirelionel@gmail.com>
2023-07-12 23:19:25 +0200

    pycodesyt

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,7 +6,6 @@ def lookup(obj):
     """returns the list of available attributes in the obj
     args:
         obj: is the object module
-    
     return:
         lsi of vabalable attr"""
     return dir(obj)
