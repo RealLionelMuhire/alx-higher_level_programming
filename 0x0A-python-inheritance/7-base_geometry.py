ba3a987bb74c4b6c0a0f05ea1ba220b5bfc2beab
RealLionelMuhire <muhirelionel@gmail.com>
2023-07-13 00:57:51 +0200

    geo name

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -17,6 +17,6 @@ class BaseGeometry:
         Return: validated
         """
         if not isinstance(value, int):
-            raise TypeError("<name> must be an integer")
+            raise TypeError("{name} must be an integer")
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("{name} must be greater than 0")
