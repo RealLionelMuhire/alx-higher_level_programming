3506945a2e859573b9e90fa8c68d521911e2c5c2
RealLionelMuhire <muhirelionel@gmail.com>
2023-07-13 14:02:20 +0200

    reads file

---
+++ 0x0B-python-input_outpu/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""reads the file and prints"""
+
+
+def read_file(filename=""):
+    """reads the file and prints output
+        args:
+            filename
+    """
+    with open(filename, 'r') as file:
+        content = file.read()
+        print(content)
---
+++ 0x0B-python-input_outpu/README.md
@@ -0,0 +1,1 @@
+This project is about file IO and JSON in alx
