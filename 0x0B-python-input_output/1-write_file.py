d433ade5920285f52b4da3bb062c319020fc5ca9
RealLionelMuhire <muhirelionel@gmail.com>
2023-07-13 15:10:46 +0200

    djh

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""write"""
+
+
 def write_file(filename="", text=""):
+    """wirte and return number of char written"""
+
     with open(filename, 'w') as file:
         return file.write(text)
