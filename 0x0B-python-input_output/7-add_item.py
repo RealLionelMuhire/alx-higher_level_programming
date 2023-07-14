8a6d02011d57c07a497faf0a25ab715d8c0b215c
RealLionelMuhire <muhirelionel@gmail.com>
2023-07-14 15:53:32 +0200

    hjajscb

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Importing json and extend"""
+"""Add all  arguments to a list and save them to a file"""
 import sys
 if __name__ == "__main__":
     save_to_json = __import__('5-save_to_json_file').save_to_json_file
