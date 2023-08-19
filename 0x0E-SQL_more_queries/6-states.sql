f21bb9625fca186574d6effaad6d3596c00d042e
RealLionelMuhire <muhirelionel@gmail.com>
2023-08-19 23:14:57 +0200

    states and incremeneting id

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- states auto generated id and uniq
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+	PRIMARY KEY(id),
+	id INT AUTO_INCREMENT NOT NULL,
+	name VARCHAR(256) NOT NULL
+	);
