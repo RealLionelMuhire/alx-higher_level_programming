74fefd8421a019bd3b8d8eecbb45e34e28e99c30
RealLionelMuhire <muhirelionel@gmail.com>
2023-08-19 21:00:33 +0200

    adding pwd and priveledges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
--- creating the user
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'Muhire07*';
+-- creating the user and priviledges with identifications
+CREATE USER IF NOT EXISTS  user_0d_1@localhost IDENTIFIED BY 'User_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@localhost;
