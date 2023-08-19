6bbd4440f759c0af022897feb78a5169bbb90743
RealLionelMuhire <muhirelionel@gmail.com>
2023-08-19 23:53:06 +0200

    adding some in operator

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- calfonians cities
 SELECT id, names FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'Calfornia')
+WHERE state_id IN (SELECT id FROM states WHERE name = 'Calfornia')
 ORDER BY id ASC;
