212b9fe8ce3ee9b492c30fcf92fc82f269a78cb3
RealLionelMuhire <muhirelionel@gmail.com>
2023-09-18 00:58:44 +0200

    mysql adding some cities

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,22 +1,23 @@
-#!/usr/bin/pyton3
+#!/usr/bin/python3
 
 """inserting cities"""
 
 
 def main():
-    import MyMSQLdb
-    import sys import argv
+    import MySQLdb
+    from sys import argv
 
     db = MySQLdb.connect(host='localhost', user=argv[1], passwd=argv[2],
-                         db=argv[3],port=3306)
+                         db=argv[3], port=3306)
     
     cur = db.cursor()
     cur.execute(
-        ""
+        "SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN\
+        states ON cities.id = states.id ORDER BY cities.id ASC;"
     )
     cur.close()
     db.close()
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     main()
